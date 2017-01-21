7a179b071102af030455e97f5058bc70b5b51b0d
Kohei Tabata <nerd0geek1@gmail.com>
2017-01-21 15:49:02 +0900

    add project

---
+++ PhotoDownloaderSwift/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  PhotoDownloaderSwift
+//
+//  Created by Kohei Tabata on 1/21/17.
+//  Copyright © 2017 Kohei Tabata. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
